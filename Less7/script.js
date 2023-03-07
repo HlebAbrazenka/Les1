69d013594781c5ba9dacf50170b5d94a7b598107
Hleb Abrazenka <dr.bread92@gmail.com>
2023-03-07 22:01:14 +0100

    new file:   Less7/index.html 	new file:   Less7/script.js

---
+++ Less7/script.js
@@ -0,0 +1,47 @@
+console.log('Hello world!');
+
+// let num = 7;
+// let name = 'Hleb';
+
+// if (){
+//     let last_name = "Abrazenka";
+
+// }
+// console.log(last_name);
+
+//let num = 4;
+//let num = 5; нельзя к одному значению
+
+//const pi = 3.14; постоянные
+
+//let greeting = "Hi";
+//console.log (greeting); выводит текст
+
+//let result = prompt('enter');
+//console.log(result); вводит текст на стр в браузере
+
+//let r = 12;
+//let g = 34;
+//let b = 14;
+//console.log('rgb(' + r + ', ' + g + ', ' + b + ')'); выведение цифр конкатенацией
+
+//let num = 5;
+//console.log(typeof(num));
+
+//let age = prompt('how old are you');
+//console.log(typeof(age));
+
+//let num2 = 69;
+//console.log(String(num2));
+
+//console.log('3' + 3); =33
+
+//console.log(+'3' + 3); =6
+
+//console.log('9' / 3); =3
+
+//let num1 = Number(prompt('Enter the number'));
+//let num2 = Number(prompt('Enter the number'));
+//let sum = num1 + num2;
+//console.log(sum);
+
