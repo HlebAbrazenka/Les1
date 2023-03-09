6207c338da7de2f720bc5311ecbf7e3bede8aac9
Hleb Abrazenka <dr.bread92@gmail.com>
2023-03-09 22:01:27 +0100

    new file:   Less8/index.html 	new file:   Less8/script.js

---
+++ Less8/script.js
@@ -0,0 +1,94 @@
+//let age = prompt("HOW OLD ARE YOU?");
+
+//if(age == 18){
+  //  console.log("OK");
+//}
+//else{
+  //  console.log("NO")
+//}
+
+//if(age == 18){
+//    console.log("OK");
+//}
+
+// let num = prompt("Enter the number");
+
+// if(num == 0){
+// console.log('num = 0');
+// }
+// else if(num > 0){
+// console.log('num > 0');
+// }
+// else{
+// console.log('num < 0');
+// }
+    
+//let num1 = Number(prompt("Enter the number-1"));
+//let num2 = Number(prompt("Enter the number-2"));
+
+//if(num1 > num2){
+//console.log(num1);
+//}
+
+//else if (num1=num2){
+  //  console.log("+");
+//}
+
+//else{
+//console.log(num2);
+//}
+
+// let num1 = Number(prompt("Enter the number"));
+
+
+// if(num1 > 0){
+// console.log('plus');
+// }
+
+// else if(num1 = 0){
+// console.log("0");
+// }
+
+// else{
+// console.log("minus");
+// }
+
+//let fruits = ["Apple" , "Banan"];
+// console.log(fruits.length); //длина/кол-во массива
+//console.log(fruits[0]);
+//fruits.push ("Orange");
+//console.log(fruits[fruits.length - 1])
+
+// let styles = ["Jazz" , "Bluze"];
+// styles.push("Rock-n-Roll");
+// styles [1] = "Classik";
+// styles.shift();
+// styles.unshift("Rap" , "Reggie");
+
+// console.log(styles);
+
+// let num = [1, 4, 2, 5, 3];
+
+// let pow = [num[0]**2, num[1]**2, num[2]**2, num[3]**2, num[4]**2, ]
+
+// console.log(pow);
+
+
+let num = [1, 4, 2, 5, 3];
+let pow = [];
+for(let i = 0; i < num.length; i++){
+pow.push(num[i]**2);
+}
+console.log(num);
+console.log(pow);
+
+
+
+
+
+
+
+
+
+
+
