09bef30a5c8f9d402953081debd6676a4120894e
Hleb Abrazenka <dr.bread92@gmail.com>
2023-03-12 19:17:14 +0100

    new file:   HomeWork/HW5/index.html 	new file:   HomeWork/HW5/script.js

---
+++ HomeWork/HW5/script.js
@@ -0,0 +1,116 @@
+// //1 задание//
+// let d = 7;
+// console.log (d);
+// console.log (d **2);
+// console.log (d **3);
+// //1 задание//
+
+// //2 задание//
+// let num = prompt ("Enter the number");
+// if (num > 0){
+// console.log (num **2);
+// }
+// else{
+//     console.log ("This number <= 0")
+// }
+// //2 задание//
+
+// //3 задание//
+// let num1 = Number(prompt ("First number"));
+// let num2 = Number(prompt ("Second number"));
+
+// if (num1 > num2){
+//     console.log (num1 + num2);
+// }
+// else if (num1 < num2){
+//     console.log (num1 * num2);
+// }
+// else{
+//     console.log ("First number = Second number");
+// }
+// //3 задание//
+
+// //4 задание//
+// let num = Number(prompt ("Number"));
+
+// if(num < 0){
+//     console.log (num **2)
+// }
+// else{
+//     console.log ("Error");
+// }
+// //4 задание//
+
+// //5 задание//
+// let num1 = Number(prompt ("Число учеников"));
+// let num2 = Number(prompt ("Число стульев"));
+
+// if(num2 >= num1){
+//     console.log ("Стульев хватает");
+// }
+// else{
+//     console.log ("Стульев не хватает");
+// }
+// //5 задание//
+
+// //6 задание//
+// let num = [1, 3, -2, 5, -4, 10];
+// console.log ("Start");
+// for(let i = 0; i < num.length; i++){
+    
+//     if(num[i] > 0){ 
+//     if(num[i] < 10)
+// console.log(num[i])
+// }
+// }
+// console.log ("End");
+// //6 задание//
+
+// //7 задание//
+// let num = [10, 15, -20, 5, 25];
+
+// for(let i = 0; i < num.length; i++){
+
+//     if(num[i] >= 10){ 
+//     console.log(num[i])
+//  }
+//  }
+// //7 задание//
+
+// //8 задание//
+// let num = [10, 15, -20, 23, 25, 28, 14];
+// for(let i = 0; i < num.length; i++){
+//     if(num[i] >= 15){ 
+//     console.log(num[i])
+//      }
+// }
+// //8 задание//
+
+// //9 задание//
+// let num = [10, 15, -20, 23, -25, 28, 14];
+// for(let i = 0; i < num.length; i++){
+//     if(num[i] % 2){ 
+//     console.log(num[i])
+//     }
+// }
+// //9 задание//
+
+// //10 задание//
+// let num = [3, 5, 7, 9, 30, 40, 300];
+// for(let i = 0; i < num.length; i++){
+//    // if(num[i] / 3){ 
+//     if(i % 3 ==0){
+//     console.log(num[i])
+//     }
+// }
+// //10 задание//
+
+
+
+
+
+
+
+
+
+
