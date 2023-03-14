9864b1470e198e1137c45275828bcee75c96a437
Hleb Abrazenka <dr.bread92@gmail.com>
2023-03-14 22:00:03 +0100

    new file:   Less9/index.html 	new file:   Less9/script.js

---
+++ Less9/script.js
@@ -0,0 +1,99 @@
+// let user = {
+//     name: "Hleb",
+//     age: 30,
+//     'likes car' : true,
+//     'likes bird' : false
+
+// }
+// delete user.age;
+// user.isAdmin=true;
+// console.log(user);
+// console.log(user.name);
+// console.log(user.age);
+
+// //const cars = {};
+
+// let user = {
+//     name: "Hleb",
+//     surname: "Abrazenka",
+// }
+//    // user.name="Natallia"
+//    // delete user.name;
+//     console.log (user);
+
+// let codes = {
+//     '49': 'Ger',
+//     '48': 'Swei',
+//     '1': 'USA'
+// }
+// for(let key in codes){
+//     //console.log(codes[key]);
+//     //console.log(+key);
+// }
+
+// let salaries = {
+//     John: 100,
+//     Anna: 160,
+//     Pete: 130
+// }
+// let sum = 0;
+// for (let key in salaries){
+//     sum=sum+salaries[key];
+// }
+// console.log (sum);
+
+// function showMessage(){
+//     console.log('Hello!')
+// }
+// showMessage();
+
+// function function_name(parametr){
+//     //.....body.....
+// }
+
+// function sum (num1, num2){
+//     return num1+num2;
+// }
+
+// let result = sum(3,7);
+// //console.log(result);// 1 вариант
+// //console.log(sum(6,3));// 2 вариант
+
+// function check(age){
+//     if(age >= 18){
+//         return true;
+//     }
+//     return false;
+// }
+// let age = Number(prompt('How old are you?'));
+
+// if(check(age)){
+//     console.log('Доступ+');
+// }
+// else{
+//     console.log('Доступ-');
+// }
+
+// function check(num){
+//     if(num % 2 == 0){
+//         console.log("YES");
+//     }
+//     else{
+//         console.log("NO");
+//     }
+// }
+// let num = Number(prompt("Enter"));
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
