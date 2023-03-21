8a46084d966f8a78113dae72b6de7054b94b55a8
Hleb Abrazenka <dr.bread92@gmail.com>
2023-03-21 22:04:02 +0100

    new file:   Less11/index.html 	new file:   Less11/script.js 	new file:   Less11/style.css

---
+++ Less11/script.js
@@ -0,0 +1,10 @@
+// let paragraph = document.querySelector("p1");
+// let num = Number(prompt("text"));
+// if(num > 10){
+//     paragraph.classList.add("ALL");
+// }
+// console.log(paragraph.className);
+
+let paragraph = document.querySelectorAll("p1");
+paragraph.classList.remove("p1");
+console.log(paragraph);
