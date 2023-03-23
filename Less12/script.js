b5ac43ed44095766aa1f32672dddb7eebd008f07
Hleb Abrazenka <dr.bread92@gmail.com>
2023-03-23 22:08:24 +0100

    new file:   Less12/index.html 	new file:   Less12/script.js 	new file:   Less12/style.css

---
+++ Less12/script.js
@@ -0,0 +1,8 @@
+// let paragraph = document.querySelector("p")
+// paragraph.addEventListener("click", function(){alert("HI MAN")});
+
+// paragraph.addEventListener("click", myFunction1);
+// function myFunction1(){
+//     alert("Hi dude");
+// }
+
