f289ce3310930b3f195f2eee6d5e4eceeb46918f
Hleb.Abrazenka <dr.bread92@gmail.com>
2023-03-30 22:19:26 +0200

    new file:   FE-PRO/L1/index.html 	new file:   FE-PRO/L1/script.js 	new file:   FE-PRO/L1/style.css 	new file:   FE-PRO/L2/index.html 	new file:   FE-PRO/L2/script.js     new file:   FE-PRO/L2/style.css

---
+++ FE-PRO/L1/script.js
@@ -0,0 +1,14 @@
+// let a = -Infinity 
+// let b = +Infinity
+let a = 10
+let b = +Infinity
+let c = NaN
+let d = "LOL"
+let e = false
+
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+console.log(typeof(e))
+
