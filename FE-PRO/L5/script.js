f37f270352eae67738f7b0cd0848eed374d7425d
Hleb.Abrazenka <dr.bread92@gmail.com>
2023-04-23 20:50:40 +0200

    new file:   FE-PRO/L5/index.html 	new file:   FE-PRO/L5/script.js 	new file:   FE-PRO/L5/style.css

---
+++ FE-PRO/L5/script.js
@@ -0,0 +1,61 @@
+// let i = 0;
+
+// while(i < 0){
+//     console.log(i)
+//     i = i + 1
+// }
+
+// let i = 20;
+
+// while(i <= 30){
+//     console.log(i)
+//     i = i + 1
+// }
+
+// let i = 10;
+
+// while(i >= 0){
+//     console.log(i)
+//     i = i - 1
+// }
+
+// let i = 0;
+
+// while(i <= 10){
+//     console.log(i)
+//     i = i + 2
+// }
+
+// let i = -10
+
+// while(i <= 10){
+//     console.log(i)
+//     i += 1
+// }
+
+// for(let i = 0; i <= 10; i++){
+//     console.log(i)
+// }
+
+// for (let i=0; i<=10; i+=1){
+//     if(i % 2 != 0){
+//         console.log(i)
+//     }
+// }
+
+// for(let i = 0; i<=10; i+=1){
+//     if (i / 5 != 1 && i / 8 != 1) {
+//         console.log(i)    
+//     }
+// }
+// for(let i = 0; i<=10; i+=1){
+//     if (i != 5 && i !=8){
+//         console.log(i)
+//     }
+// }
+
+// for(let i = 0; i<=100; i++){
+//     if(i % Math.sqrt(i) == 0){
+//         console.log(i)
+//     }
+// }
