13c3f1f349ccd481e8718c4375c1c7f78ddbe450
Hleb.Abrazenka <dr.bread92@gmail.com>
2023-04-25 21:59:41 +0200

    new file:   FE-PRO/L6/index.html 	new file:   FE-PRO/L6/script.js 	new file:   FE-PRO/L6/style.css

---
+++ FE-PRO/L6/script.js
@@ -0,0 +1,78 @@
+// let string = "world!"
+// for(let i=0; i<string.length; i++){
+//     console.log(string[i]);
+// }
+
+// let string = "12345"
+// for(let i = string.length -1; i>-1; i--){
+//     console.log(string[i]);
+// }
+
+// console.log("-_".repeat(7));
+// console.log("HELLO WORLD!!!");
+// console.log("-_".repeat(7));
+
+
+// function sayHi(){
+// console.log("-_".repeat(7))
+// console.log('HELLO WORLD!!!')
+// console.log("-_".repeat(7))
+// }
+
+// sayHi()
+
+// function sayHi(name){
+//     console.log("-_".repeat(7))
+//     // console.log('HELLO WORLD!!! ' + name)
+//     console.log(`hello ${name}`)
+//     console.log("-_".repeat(7))
+// }
+// sayHi('Hleb')
+// sayHi('Natali')
+// sayHi('LOVE')
+
+// function sum(a,b,c,d){
+//     console.log(a+b+c+d);
+// }
+// sum(5, 10, -5, -10)
+
+
+// function sum(a,b,c){
+//     console.log(a+b+c);
+// }
+// sum(true, false, 0 )
+
+// function double(numb){
+//        //console.log(numb * numb);
+//        console.log(numb ** 2);
+// }
+// double(1)
+
+//     function checkString(str) {
+        
+//     if (typeof(str)=='string'){
+//         console.log("String+")
+//     }else{
+//         console.log("String-")
+//     }
+// }
+
+// checkString('123')
+// checkString(10)
+// checkString(true)
+// checkString('KeK')
+
+// function checkMath(num){
+// if (num % 2 == 0) {
+// console.log("+")
+// }else{
+//     console.log("-");
+// }
+// }
+// checkMath(5)
+
+
+//Тернарный оператор
+// let a = 4
+// let b = (a % 2 == 0) ?"делится без остатка": "делится с остатком"
+// console.log(b)
