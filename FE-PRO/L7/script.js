76eb22c3b53a5143180f9e13a38306c54f27dff9
Hleb.Abrazenka <dr.bread92@gmail.com>
2023-04-27 22:04:13 +0200

    new file:   FE-PRO/L7/index.html 	new file:   FE-PRO/L7/script.js 	new file:   FE-PRO/L7/style.css

---
+++ FE-PRO/L7/script.js
@@ -0,0 +1,28 @@
+// function double(num){
+//     console.log(num ** 2);
+// }
+// double(5)
+
+// function number(num){
+//     return num * 2
+// }
+// let result = number(10)
+// console.log(result);
+
+// function handler(){
+//     console.log(100 * 5);
+// }
+// handler()
+
+// function handle(){
+//     console.log("test");
+// }
+// console.log(handle());
+
+function glc(string){
+    return string[string.length -1]
+}
+console.log(glc("HELLO"));
+console.log(glc("HLEB"));
+console.log(glc("HELL"));
+      
