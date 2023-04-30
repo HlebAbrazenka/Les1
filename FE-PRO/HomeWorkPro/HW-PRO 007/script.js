aec851ad6b4a80b4cb73363d8aac64462fbfe571
Hleb.Abrazenka <dr.bread92@gmail.com>
2023-04-30 14:01:39 +0200

    new file:   FE-PRO/HomeWorkPro/HW-PRO 007/index.html 	new file:   FE-PRO/HomeWorkPro/HW-PRO 007/script.js 	new file:   FE-PRO/HomeWorkPro/HW-PRO 007/style.css

---
+++ FE-PRO/HomeWorkPro/HW-PRO 007/script.js
@@ -0,0 +1,65 @@
+// Написать цикл, который выводит только чётные числа
+// [1,5,8,4,7,6,2,9]
+// let array = [14,1,5,8,4,7,6,2,9,3,10,12]
+// for (let i = 0; i < array.length; i+=1){
+//     if(array[i] % 2 === 0){
+//         console.log(array[i])
+//     }  
+// }
+ //-------------------------------------------------
+ 
+//Написать цикл, который выводит только положительные числа
+//и считает их сумму [1,2,3,4,5,-5,-8]
+// let array = [1,2,3,4,5,-5,-8];
+// let sum = 0;
+// for (let i=0; i<array.length; i+=1){
+//     if(arrey[i] > 0){
+//         sum = sum + array[i];
+//     }
+// }
+// console.log(sum);
+//----------------------------------------------------
+
+//Написать цикл, который выводит сумму чисел, чей индекс
+//равен значению элемента [0,1,3,5,4,6,5]
+//let array = [0,1,3,5,4,6,5];
+//let array = [0,1,2,3,4,5];
+// let sum = 0;
+// for(let i=0; i<array.length; i+=1){
+//     if(array[i]=== i){
+//         sum = sum + i;
+//     }
+// }
+// console.log(sum);
+//----------------------------------------------------
+// Написать цикл, который выводит
+// минимальное и максимальное число
+
+// let array = [1,2,3,5,8,9,52,-9,-6,10]
+// let max = -Infinity
+// let min = Infinity
+// for(let i=0; i<array.length; i+=1){
+//     const arrayElement = array[i];
+//     if(arrayElement > max){
+//         max = arrayElement;
+//     }
+//     if(arrayElement < min){
+//         min = arrayElement;
+
+//     }
+// }
+// console.log('min =', min, ';', 'max =', max);
+
+// let array = [1,2,3,100,5,8,9,-100,52,-9,-6,10];
+// let max = 1;
+// let min = 1;
+// for(let i = 0; i <array.length; i+=1){
+//     if(array[i] > min){
+//         min = array[i];
+//     }
+//     if(array[i] < max){
+//         max = array[i]
+//     }
+// }
+// console.log('min =', min, ';', 'max =', max);
+//---------------------------------------------------------
