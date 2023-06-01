99d1a6145f4f7933ac392d76abd00866c50ad7db
Hleb.Abrazenka <dr.bread92@gmail.com>
2023-06-01 22:01:30 +0200

    new file:   FE-PRO/HomeWorkPro/HW-PRO6/index.html 	new file:   FE-PRO/HomeWorkPro/HW-PRO6/script.js 	new file:   FE-PRO/L15/index.html 	new file:   FE-PRO/L15/script.js

---
+++ FE-PRO/L15/script.js
@@ -0,0 +1,30 @@
+// let car = {
+//     model: 'Tesla',
+//     mileage: 30000,
+//     year: 2020,
+//     engine: 50,
+//     broke: false
+// }
+
+// car.model = 'BMW'
+// delete car.mileage
+// car.ride = 48642
+
+// console.log(car);
+
+//------------------------------------------------------
+
+let obj = {
+    char1: 1,
+    char2: 'Windows',
+    char3: 10,
+    char4: 'Car',
+    char5: 54,
+    char6: 'String',
+}
+
+for(let elem in obj){
+    if(typeof obj[elem] === 'number'){
+    console.log(obj[elem]);
+}
+}
