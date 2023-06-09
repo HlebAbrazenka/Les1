a2e21fae7980e06116dc3881e8dc851695b8ba28
Hleb.Abrazenka <dr.bread92@gmail.com>
2023-06-09 22:00:33 +0200

    modified:   FE-PRO/HomeWorkPro/HW-PRO7/script.js

--- FE-PRO/HomeWorkPro/HW-PRO7/script.js
+++ FE-PRO/HomeWorkPro/HW-PRO7/script.js
@@ -21,9 +21,14 @@
 // key3: '3',
 // key4: '4',
 // }
-// for(let elem in someObj){
-//     console.log(elem);
+// let result = []
+// function getKeys(obj) {
+//     for(let elem in someObj){
+//        result.push(elem)
 // }
+// return console.log(result);
+// }
+// getKeys()
 
 //----------------------------------------------------------------------------
 //2)Создайте функцию getAvg(), которая получает в качестве аргумента
@@ -42,8 +47,18 @@
 // sum = sum / array.length;
 // return sum.toFixed(1);       //toFixed - преобразования числа в строку
 // }   
+// console.log(getAvg([1,2,3,4,5]));
 
-// console.log(getAvg([1,2,3,4,5,6]));
+
+// let sum = 0
+// function getAvg(array){
+//     for(let elem in array){
+//         sum += array[elem]/array.length;
+//     }
+//     return sum
+// }
+// console.log(getAvg([1,2,3,4,5]));
+
 
 
 
@@ -55,6 +70,69 @@
 // Пример: [1,true,'3','value1',9,'key']
 // Результат: 3
 //-------------------------------------------------------------------------------
+
+
+// let arr = [1,true,'3','value1','9','key']
+// let sum = 0
+// function countString(arr){
+// for(let elem of arr){
+//     if(typeof(elem) === 'string'){
+//      sum += 1;
+//     //  console.log(sum);
+//     }
+// }
+//    return console.log(sum);
+// }
+// countString(arr)
+
+// let array = ['1',true,'3','value1',9,'key']
+// let result = 0
+
+// function counterString(array){
+//     for (let elem of array){
+//         if (typeof(elem) === 'string'){
+//             result += 1;
+//         }
+//     }
+//     return console.log(result);
+// }
+// counterString(array)
+
+
+// function countString(arr) {
+//     let count = 0;
+
+//     for (let i = 0; i < arr.length; i++) {
+//       if (typeof arr[i] === 'string') {
+//         count++;
+//       }
+//     }
+
+//     console.log(count);
+//   }
+//   const array = [1, true, '3', 'value1', '9', 'key'];
+// countString(array); // Вывод: 3
+
+// let array = [1,'true','3','value1',9,'key']
+
+// function countString(arr){
+//     for(let elem in array){
+//         if(typeof array[elem] === "string"){
+//             return array[elem]
+//         }
+//     } 
+// }
+// console.log(countString(array))
+
+// let sum1 = [1,true,'3','value1',9,'key'];
+// function countString(arr){
+//     for(let elem in sum1){
+//         if(typeof (elem) === "string"){
+//             console.log(elem)
+//         }
+//     }
+// }
+// countString(sum1)
 
 
 
@@ -75,7 +153,20 @@
 //-----------------------------------------------------------------------------------
 
 
-
+// let someObj = {
+// key1: 'value1',
+// key2: 'value2',
+// key3: 'value3',
+// key4: 'value4',
+// }
+// let result = []
+// function getEntries(obj){
+//     for(let elem in obj){
+//     result.push([elem, obj[elem]])
+//     }
+//     return result 
+// }
+// console.log(getEntries(someObj));
 
 //-------------------------------------------------------------------------------------
 //5)Задан массив array, передающий произвольное количество элементов. 
@@ -97,7 +188,29 @@
 //----------------------------------------------------------------------------------------
 
 
+//   array = [1,true,'3','value1',9,'key', 0];
 
+//   function arrayToObject(arr) {
+//       let obj = {};
+//       for (let i = 0; i < arr.length; i++) {
+//         obj['key' + (i + 1)] = arr[i];
+//       }
+//       return obj;
+//     }
+//     let  result = arrayToObject(array);
+//     console.log(result);
+
+
+const array = [1,true,'3','value1',9,'key', 0];
+function object(){
+let obj = {}
+for(let elem in array ){
+ obj['key'+ ++elem]  = array[elem -1] 
+}
+console.log(obj);
+return obj 
+}
+object()
 
 //-----------------------------------------------------------------------------------------
 
