9636adfeff463e3dd1d74eb706ba23001fb1d52a
Hleb.Abrazenka <dr.bread92@gmail.com>
2023-06-22 22:08:07 +0200

    new file:   FE-PRO/L21/index.html 	new file:   FE-PRO/L21/script.js 	new file:   FE-PRO/L21/style.css

---
+++ FE-PRO/L21/script.js
@@ -0,0 +1,14 @@
+// console.log(123);
+
+// let div_elem = document.querySelector('#root')
+
+// div_elem.className = div_elem.id
+
+// div_elem.id = 'Hleb'
+// console.log(div_elem);
+
+let p_elem = document.querySelector('.text')
+p_elem.setAttribute('name','p_text')
+p_elem.innerText = 'Hello world'
+
+console.log(p_elem);
